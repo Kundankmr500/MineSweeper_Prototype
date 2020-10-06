8f5ce7f12a27bf0f055c3dd157027bb0c617a531
Kundan kumar <30592698+Kundankmr500@users.noreply.github.com>
2020-10-06 18:11:11 +0530

    Initial commit

---
+++ Assets/Minesweeper/Scripts/UI_PlayAgain.cs
@@ -0,0 +1,38 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading this package
+    I hope you find it useful in your projects
+    If you have any questions let me know
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using CodeMonkey.Utils;
+
+public class UI_PlayAgain : MonoBehaviour {
+
+    public static UI_PlayAgain Instance { get; private set; }
+
+    private void Awake() {
+        Instance = this;
+        transform.Find("playAgainBtn").GetComponent<Button_UI>().ClickFunc = () => SceneManager.LoadScene(0);
+        Hide();
+    }
+
+    public void Hide() {
+        gameObject.SetActive(false);
+    }
+
+    public void Show() {
+        gameObject.SetActive(true);
+        transform.SetAsLastSibling();
+    }
+
+}
---
+++ Assets/Scripts/GameService.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameService : MonoBehaviour
+{
+    public int RowSize;
+    public int ColSize;
+    public int MineCount;
+
+    private GameObject[,] Grid;
+
+
+    void Start()
+    {
+        Grid = new GameObject[RowSize, ColSize];
+
+        for (int i = 0; i < MineCount; i++)
+        {
+            PlaceMine();
+        }
+
+    }
+
+
+    private void PlaceMine()
+    {
+        int XIndex = Random.Range(0, RowSize);
+        int YIndex = Random.Range(0, ColSize);
+
+        if(Grid[XIndex,YIndex] == null)
+        {
+            GameObject empty = new GameObject();
+            Grid[XIndex, YIndex] = empty;
+
+            Debug.Log("( " + XIndex + " , " + YIndex + " )");
+        }
+        else
+        {
+            PlaceMine();
+        }
+    }
+}
